652a4efa389ed5a6672964f3633b369b1fe4e2d3
Scott Ryder <swryder@gmail.com>
2020-08-23 13:26:50 -0700

    Adding Project File

---
+++ Main Encounter/Shared/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Scott Ryder on 8/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ Main Encounter/Shared/Main_EncounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Main_EncounterApp.swift
+//  Shared
+//
+//  Created by Scott Ryder on 8/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Main_EncounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
